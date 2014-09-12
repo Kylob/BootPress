d6b4178411bf8343ad7d6b0bc748881f540804da
paralogizing <kyle.gadd@gmail.com>
2014-09-11 17:42:08 -0800

    Added .readme

---
+++ plugins/Image/Image.php
@@ -0,0 +1,160 @@
+<?php
+
+class Image {
+
+  public $magick;
+  public $type;
+  public $width;
+  public $height;
+  public $source = ''; // the original file uri
+  public $saved = ''; // the last saved file uri
+  private $crop = ''; // -crop command line
+  private $cropped = false; // an array of data if image is cropped
+  private $resize = false; // an array(width, height) if image is resized
+  
+  public function __construct ($uri) {
+    if (file_exists($uri)) {
+      $this->magick = (function_exists('ImageMagick')) ? true : false;
+      list($width, $height, $type) = @getimagesize($uri);
+      switch ($type) {
+        case 1: $this->type = 'gif'; break; // do not crop and resize well from jpegs using ImageMagick
+        case 2: $this->type = 'jpg'; break;
+        case 3: $this->type = 'png'; break;
+        case 17: if ($this->magick) $this->type = 'ico'; break; // 255 pixels is the max width and height
+      }
+      if ($this->type) { // continue
+        $this->width = $width;
+        $this->height = $height;
+        $this->source = $uri;
+      }
+    }
+  }
+  
+  public function crop ($coords='') {
+    if (!is_array($coords)) $coords = explode(',', $coords);
+    if (count($coords) == 4) {
+      list($x1, $y1, $x2, $y2) = $coords;
+      $width = $x2 - $x1;
+      $height = $y2 - $y1;
+      $this->crop = " -crop {$width}x{$height}+{$x1}+{$y1}";
+      $this->cropped = array('width'=>$width, 'height'=>$height, 'x1'=>$x1, 'y1'=>$y1, 'x2'=>$x2, 'y2'=>$y2);
+    }
+  }
+  
+  public function resize ($width, $height=0, $enforce=false) {
+    if (empty($height)) $height = round($this->max('height') / $this->max('width') * $width); // ie. keep the same aspect ratio
+    if ($enforce === false) { // We want to constrain proportions within $width and $height
+      $use = (($width / $height) <= ($this->max('width') / $this->max('height'))) ? 'width' : 'height';
+      $max_dimension = ($use == 'width') ? $width : $height;
+      $ratio = ($this->max('width') >= $this->max('height')) ? $max_dimension / $this->max('width') : $max_dimension / $this->max('height');
+      if($this->max('width') > $max_dimension || $this->max('height') > $max_dimension) {
+        $width = round($this->max('width') * $ratio);
+        $height = round($this->max('height') * $ratio);
+      } else { // the resize is larger than the original
+        $width = $this->max('width');
+        $height = $this->max('height');
+      }
+    } // Otherwise we want exactly $width and $height
+    $this->resize = array($width, $height);
+  }
+  
+  public function square ($pixels, $enforce=false) {
+    $requested = $pixels;
+    $width = $this->max('width');
+    $height = $this->max('height');
+    if ($pixels > $width || $pixels > $height) $pixels = min($width, $height);
+    $x = $y = 0;
+    if ($width > $height) { // keep the height
+      $x = round(($width - $height) / 2);
+      if ($this->cropped) {
+        $x += $this->cropped['x1'];
+        $y = $this->cropped['y1'];
+      }
+      $this->crop(array($x, $y, $x+$height, $y+$height));
+    } else { // keep the width
+      $y = round(($height - $width) / 2);
+      if ($this->cropped) {
+        $y += $this->cropped['y1'];
+        $x = $this->cropped['x1'];
+      }
+      $this->crop(array($x, $y, $x+$width, $y+$width));
+    }
+    if ($enforce !== false) $pixels = $requested;
+    $this->resize($pixels, $pixels, 'enforce');
+  }
+  
+  public function save ($uri, $quality=80) {
+    if (empty($this->source)) return false;
+    if (!is_dir(dirname($uri))) mkdir(dirname($uri), 0755, true);
+    $saved = false;
+    if ($this->magick) {
+      $convert = $this->crop;
+      if ($this->resize) {
+        list($width, $height) = $this->resize;
+        $convert .= " -resize {$width}x{$height}!";
+      }
+      $image = ImageMagick('convert', "\"{$this->source}\"{$convert} -quality {$quality} -strip -verbose \"{$uri}\"");
+      if ($image['return'] == 0) $saved = true;
+    } elseif ($this->type != 'ico') { // in case $this->magick was set to false
+      if (!empty($this->resize)) {
+        list($width, $height) = $this->resize;
+      } elseif (!empty($this->cropped)) {
+        list($width, $height) = array_values($this->cropped);
+      } else {
+        $width = $this->width;
+        $height = $this->height;
+      }
+      $image = imagecreatetruecolor($width, $height);
+      if ($this->type == 'png') {
+        imagealphablending ($image, false);
+        imagesavealpha ($image, true);
+        $transparent = imagecolorallocatealpha ($image, 255, 255, 255, 127);
+        imagefilledrectangle ($image, 0, 0, $width, $height, $transparent);
+      } else {
+        $white = imagecolorallocate ($image, 255, 255, 255);
+        imagefilledrectangle ($image, 0, 0, $width, $height, $white);
+      }
+      switch ($this->type) {
+        case 'gif': $source = imagecreatefromgif($this->source); break;
+        case 'jpg': $source = imagecreatefromjpeg($this->source); break;
+        case 'png': $source = imagecreatefrompng($this->source); break;
+      }
+      $src_x = ($this->cropped) ? $this->cropped['x1'] : 0;
+      $src_y = ($this->cropped) ? $this->cropped['y1'] : 0;
+      imagecopyresampled ($image, $source, 0, 0, $src_x, $src_y, $width, $height, $this->max('width'), $this->max('height'));
+      switch (substr($uri, strrpos($uri, '.') + 1)) {
+        case 'gif': $saved = imagegif($image, $uri); break;
+        case 'jpeg':
+        case 'jpg': $saved = imagejpeg($image, $uri, $quality); break;
+        case 'png':
+          if ($quality >= 90) {
+            $quality = 0;
+          } else {
+            $quality = abs(round($quality / 10) - 9);
+          }
+          $saved = imagepng ($image, $uri, $quality);
+          break;
+      }
+      unset($image, $source);
+    }
+    if ($saved) $this->saved = $uri;
+    return $saved;
+  }
+  
+  private function max ($dimension) {
+    switch ($dimension) {
+      case 'width': return ($this->cropped) ? $this->cropped['width'] : $this->width; break;
+      case 'height': return ($this->cropped) ? $this->cropped['height'] : $this->height; break;
+    }
+  }
+  
+  static function data ($uri) {
+    if (!file_exists($uri)) return '';
+    $type = substr($uri, strrpos($uri, '.') + 1);
+    $mime = ($type == 'jpg') ? 'jpeg' : $type;
+    return "data:{$mime};base64," . base64_encode(file_get_contents($uri));
+  }
+  
+}
+
+?>
---
+++ plugins/Image/Magick.php
@@ -0,0 +1,27 @@
+<?php
+
+function ImageMagick ($command, $params, $dir='') {
+  $current = getcwd();
+  if (!empty($dir)) chdir($dir);
+  $cmd = array(escapeshellarg(IMAGEMAGICK_PATH . $command));
+  if (is_array($params)) {
+    foreach ($params as $key => $value) {
+      if (strpos($value, BASE) !== false) $value = escapeshellarg($value);
+      if (!is_numeric($key)) {
+        $cmd[] = '-' . $key . ' ' . $value;
+      } else {
+        $cmd[] = $value;
+      }
+    }
+  } else {
+    $cmd[] = $params;
+  }
+  $return = null;
+  $output = array();
+  $cmd = implode(' ', $cmd);
+  exec($cmd . ' 2>&1', $output, $return);
+  chdir($current);
+  return array('cmd'=>$cmd, 'output'=>$output, 'return'=>$return);
+}
+
+?>
