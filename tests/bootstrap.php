c353cdfc838a7012f3bd152312c03a75d6ae559e
Kyle Gadd <kyle.gadd@gmail.com>
2016-08-04 22:54:10 -0800

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,8 +2,7 @@
 
 // Enable Composer autoloader
 /** @var \Composer\Autoload\ClassLoader $autoloader */
-$autoloader = require '../vendor/autoload.php';
+$autoloader = require dirname(__DIR__) . '/vendor/autoload.php';
 
 // Register test classes
 $autoloader->addPsr4('BootPress\Tests\\', __DIR__);
-$autoloader->addPsr4('BootPress\\', dirname(__DIR__).'/src');
