39cd327cc5820b3582171308e9d891afb80e3d0d
Kyle Gadd <kyle.gadd@gmail.com>
2016-08-05 14:41:51 -0800

    Update BlogTest.php

--- tests/BlogTest.php
+++ tests/BlogTest.php
@@ -133,6 +133,9 @@ class BlogTest extends HTMLUnit_Framework_TestCase
     public function testAboutPage()
     {
         $template = $this->blogPage('about.html');
+        
+        echo file_get_contents(static::$config);
+        
         $file = static::$folder.'about/index.tpl';
         ##
         #  {*
