234560bc0fcf85a456a0a111af9cf888c4c74939
Kyle Gadd <kyle.gadd@gmail.com>
2016-08-07 05:56:19 -0800

    Update Component.php

--- src/Asset/Component.php
+++ src/Asset/Component.php
@@ -307,7 +307,7 @@ class Component
         $base = strlen($page->url['base']);
         
         if ($array) {
-            print_r([static::$not_found, $cache, $assets]);
+            print_r([$matches, $page->dir, static::$not_found, $cache, $assets]);
         }
         
         foreach ($assets as $match => $url) {
