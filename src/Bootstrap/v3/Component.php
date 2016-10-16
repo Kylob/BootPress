2c33e63cb62542c7555c0a88f94977fdbbeb40eb
Kyle Gadd <kyle.gadd@gmail.com>
2016-10-16 05:20:38 -0800

    Update Component.php

--- src/Bootstrap/v3/Component.php
+++ src/Bootstrap/v3/Component.php
@@ -4,10 +4,5 @@ namespace BootPress\Bootstrap\v3;
 
 class Component extends \BootPress\Bootstrap\Common
 {
-    public $version;
-    public function __construct()
-    {
-        $this->version = 3;
-        parent::__construct();
-    }
+    public $version = 3;
 }
