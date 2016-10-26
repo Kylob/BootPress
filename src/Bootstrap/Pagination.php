b5db68285c2b68243ae79788d10560caff6f1b92
Kyle Gadd <kyle.gadd@gmail.com>
2016-10-26 15:37:32 -0800

    Update Pagination.php

--- src/Bootstrap/Pagination.php
+++ src/Bootstrap/Pagination.php
@@ -4,6 +4,23 @@ namespace BootPress\Bootstrap;
 
 class Pagination extends \BootPress\Pagination\Component
 {
+    /**
+     * Returns a BootPress\Pagination\Component with the '**bootstrap**' defaults.
+     *
+     * @return object
+     *
+     * ```php
+     * $records = range(1, 100);
+     *
+     * if (!$bp->pagination->set('page', 10, 'http://example.com')) {
+     *     $bp->pagination->total(count($records));
+     * }
+     *
+     * echo $pagination->links();
+     *
+     * echo $pagination->pager();
+     * ```
+     */
     public function __construct()
     {
         parent::__construct('bootstrap');
