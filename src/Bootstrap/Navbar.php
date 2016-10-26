36c0ca4b3f5872d0f0b81ef804082b40eb679a1b
Kyle Gadd <kyle.gadd@gmail.com>
2016-10-26 13:49:03 -0800

    Update Navbar.php

--- src/Bootstrap/Navbar.php
+++ src/Bootstrap/Navbar.php
@@ -8,13 +8,13 @@ class Navbar extends Common
      * Create a new navbar.
      * 
      * @param mixed  $brand   The name of your website. If this is a string then it will automatically link to your ``$page->url['base']``. If you want to override that, then make this an ``array($brand => $link)``.
-     * @param string $align   Either '**top**', '**bottom**', or '**static**' if you want to fix the alignment. If you are just trying to get to the next arg then you can declare '**inverse**' here and we will know what you are talking about.
+     * @param string $align   Either '**top**', '**bottom**', or '**static**' if you want to fix the alignment. If you're just trying to get to the next arg then you can declare '**inverse**' here, and we'll know what you're talking about.
      * @param mixed  $inverse If this is anything but false (eg. '**inverse**') then we will display the inverted or alternate navbar.
      * 
      * @return string
      * 
      * ```php
-     * echo $bp->navbar->open(array('Website', 'http://example.com'));
+     * echo $bp->navbar->open(array('Website' => 'http://example.com'));
      * ```
      */
     public function open($brand, $align = '', $inverse = false)
@@ -56,14 +56,14 @@ class Navbar extends Common
     }
 
     /**
-     * This will create a menu of links across your navbar.
+     * Create a menu of links across your navbar.
      * 
      * @param array $links   An ``array($name => $href, ...)`` of links. If ``$href`` is an array unto itself, then it will be turned into a dropdown menu with the same header and divider rules applied as with buttons.
      * @param array $options The options available here are:
      * 
-     * - '**active**' => $name, $href, 'url', 'urlquery', or number (starting from 1)
-     * - '**disabled**' => $name or $href or number (starting from 1)
-     * - '**pull**' => 'left' (default) or 'right'
+     * - '**active**' => **$name**, **$href**, '**url**', '**urlquery**', or number (starting from 1)
+     * - '**disabled**' => **$name** or **$href** or number (starting from 1)
+     * - '**pull**' => '**left**' (default) or '**right**'
      * 
      * @return string
      * 
@@ -75,10 +75,12 @@ class Navbar extends Common
      *         'Action' => '#',
      *         'More' => '#',
      *     ),
-     * ), array('active'=>'Home'));
+     * ), array(
+     *     'active' => 'Home',
+     * ));
      * ```
      */
-    public function menu(array $links, array $options = array()) // array('active'=>'name or url', 'pull'=>'left or right')
+    public function menu(array $links, array $options = array())
     {
         $align = (isset($options['pull'])) ? ' navbar-'.$options['pull'] : '';
         unset($options['pull']);
@@ -87,13 +89,19 @@ class Navbar extends Common
     }
 
     /**
-     * This is used in exactly the same manner as $bp->button(...), except that it will be added to the navbar, and receive the navbar-btn class.
+     * Exactly the same as ``$bp->button()``, except that it will receive a '**navbar-btn**' class.
      * 
-     * @param string $class
-     * @param string $name
-     * @param array  $options
+     * @param string $class   To pull it one way or the other, you can add the class '**navbar-right** or '**navbar-left**'.
+     * @param string $name    The text of your button.
+     * @param array  $options You can also pull the button here by adding ``array('pull' => 'right')`` or ``array('pull' => 'left')``
      * 
      * @return string
+     *
+     * ```php
+     * echo $bp->navbar->button('primary', 'Sign In', array(
+     *     'pull' => 'right',
+     * ));
+     * ```
      */
     public function button($class, $name, array $options = array())
     {
@@ -107,15 +115,17 @@ class Navbar extends Common
     }
 
     /**
-     * This is used in exactly the same manner as ``$bp->search(...)``, but modified slightly for use in a navbar.
+     * Exactly the same as ``$bp->search()``, except the ``<form>`` will receive a **'navbar-form navbar-right'** class.
      * 
-     * @param string $url  The same as it's parent.
-     * @param array  $form The same as it's parent, except for the default '**class**' which is '**navbar-form navbar-right**'.
+     * @param string $url  Where to send the search term.
+     * @param array  $form You can pull the form left by adding ``array('class' => 'navbar-form navbar-left')``.
      * 
      * @return string
      * 
      * ```php
-     * echo $bp->navbar->search($page->url());
+     * echo $bp->navbar->search('http://example.com/', array(
+     *     'button' => false,
+     * ));
      * ```
      */
     public function search($url, array $form = array())
@@ -128,9 +138,9 @@ class Navbar extends Common
     }
 
     /**
-     * This adds a string of text to the navbar. It will be wrapped in a ``<p>`` tag, and any ``<a>``'s it finds will also receive their special class treatment.
+     * Add a string of text to your navbar.
      * 
-     * @param string $string The message you would like to get across.
+     * @param string $string The message you would like to get across.  It will be wrapped in a ``<p class="navbar-text">`` tag, and any ``<a>``'s  will be classed with a '**navbar-link**'.
      * @param string $pull   Either '**left**' or '**right**'.
      * 
      * @return string
@@ -147,7 +157,7 @@ class Navbar extends Common
     }
 
     /**
-     * This method will add the final ``<div>``'s to your navbar.
+     * Add the final ``</div>``'s and ``</nav>``.
      * 
      * @return string
      * 
