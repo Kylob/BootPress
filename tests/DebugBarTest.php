6987e495d283ee133b2d51418afc9d2f12fba9a8
Kyle Gadd <kyle.gadd@gmail.com>
2017-01-03 20:11:45 -0900

    Update DebugBarTest.php

--- tests/DebugBarTest.php
+++ tests/DebugBarTest.php
@@ -8,15 +8,6 @@ use Symfony\Component\HttpFoundation\Request;
 class DebugBarTest extends \BootPress\HTMLUnit\Component
 {
     
-    public static function setUpBeforeClass()
-    {
-        // To avert a ps_files_cleanup_dir permission denied error.
-        if (is_dir('/tmp')) {
-            session_save_path('/tmp');
-        }
-        ini_set('session.gc_probability', 0);
-    }
-
     public function testFunctionExists()
     {
         $this->assertTrue(function_exists('debugbar'));
