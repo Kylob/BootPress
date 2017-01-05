fa846bcb612105e1cbbf7abc05993a1a598dcff4
Kyle Gadd <kyle.gadd@gmail.com>
2017-01-04 20:12:00 -0900

    Update DataFormatter.php

--- src/DebugBar/DataFormatter.php
+++ src/DebugBar/DataFormatter.php
@@ -2,23 +2,11 @@
 
 namespace BootPress\DebugBar;
 
+use BootPress\Blog\Twig\Theme;
 use DebugBar\DataFormatter\DataFormatterInterface;
-use Symfony\Component\VarDumper\Cloner\VarCloner;
-use Symfony\Component\VarDumper\Dumper\CliDumper;
 
 class DataFormatter implements DataFormatterInterface
 {
-    /**
-     * DataFormatter constructor.
-     */
-    public function __construct()
-    {
-        $this->cloner = new VarCloner();
-        $this->cloner->setMaxItems(100);
-        $this->cloner->setMaxString(100);
-        $this->dumper = new CliDumper();
-    }
-
     /**
      * @param $data
      *
@@ -26,20 +14,10 @@ class DataFormatter implements DataFormatterInterface
      */
     public function formatVar($data)
     {
-        $output = '';
-
-        $this->dumper->dump(
-            $this->cloner->cloneVar($data),
-            function ($line, $depth) use (&$output) {
-                // A negative depth means "end of dump"
-                if ($depth >= 0) {
-                    // Add four spaces of indentation to the line
-                    $output .= str_repeat("\t", $depth).$line."\n";
-                }
-            }
-        );
-
-        return trim($output);
+        return Theme::dumper($data, array(
+            'dumper' => 'cli',
+            'indent' => "\t",
+        ));
     }
 
     /**
