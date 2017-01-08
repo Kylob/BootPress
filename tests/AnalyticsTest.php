948312bc7ea5960f5626006ffc7065a5c8ae4475
Kyle Gadd <kyle.gadd@gmail.com>
2017-01-08 07:57:33 -0900

    Update AnalyticsTest.php

--- tests/AnalyticsTest.php
+++ tests/AnalyticsTest.php
@@ -19,13 +19,16 @@ class AnalyticsTest extends \BootPress\HTMLUnit\Component
         }
         ini_set('session.gc_probability', 0);
         self::$page = array('testing' => true, 'dir' => __DIR__.'/page', 'suffix' => '.html');
+        self::tearDownAfterClass();
     }
 
     public static function tearDownAfterClass()
     {
-        $db = self::$page['dir'].'/Analytics.db';
-        if (is_file($db)) {
-            @unlink($db);
+        foreach (array('Analytics.db', 'analytics.csv', 'analytics-temp.csv') as $file) {
+            $file = self::$page['dir'].'/'.$file;
+            if (is_file($file)) {
+                @unlink($file);
+            }
         }
     }
 
@@ -34,9 +37,6 @@ class AnalyticsTest extends \BootPress\HTMLUnit\Component
         $page = $this->page();
 
         $db = $page->file('Analytics.db');
-        if (is_file($db)) {
-            unlink($db);
-        }
         $this->assertFileNotExists($db);
         Analytics::database();
         $this->assertFileExists($db);
@@ -44,46 +44,37 @@ class AnalyticsTest extends \BootPress\HTMLUnit\Component
 
     public function testLogProcessHitsIdAndExecMethods()
     {
-        ob_start();
-        
         // Robot - Google
         $page = $this->page(array('server' => array(
             'HTTP_USER_AGENT' => 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
         )));
         $csv = $page->file('analytics.csv');
-        if (is_file($csv)) {
-            unlink($csv);
-        }
         $this->assertFileNotExists($csv);
         $this->assertFalse(Analytics::log());
+        $page->send('');
         $this->assertFileExists($csv);
-        $page->send('Content');
 
         // Mobile - iPhone
-        $_SESSION = array();
-        session_regenerate_id(); // so it doesn't think we are a repeat visitor
         $page = $this->page(array('server' => array(
             'HTTP_USER_AGENT' => 'Mozilla/5.0 (iPhone; U; ru; CPU iPhone OS 4_2_1 like Mac OS X; ru) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148a Safari/6533.18.5',
         )));
         $this->assertFalse(Analytics::log());
-        $page->send('Content');
+        $page->send('');
 
         // Desktop - OS X 10_6_8
-        $_SESSION = array();
-        session_regenerate_id(); // so it doesn't think we are a repeat visitor
         $page = $this->page(array('server' => array(
             'HTTP_USER_AGENT' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.13+ (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2',
             'HTTP_REFERER' => 'http://somewhere-else.com/linked.html?query=string',
         )));
         $this->assertFalse(Analytics::log());
-        $page->send('Content');
+        $page->send('');
 
         // check that everything was logged properly
         $file = file($csv);
-        $this->assertCount(6, $file);
-        foreach (array('analytics', 'bots', 'analytics', 'bots', 'analytics', 'bots') as $table) {
-            $row = array_shift($file);
-            $this->assertEquals($table, strstr($row, ',', true));
+        $this->assertCount(4, $file);
+        foreach (array('bots', 'bots', 'bots', 'analytics') as $expected) {
+            list($table, $started) = str_getcsv(array_shift($file).',,,,,');
+            $this->assertEquals($expected, $table);
         }
 
         // process the first round of hits
@@ -111,9 +102,16 @@ class AnalyticsTest extends \BootPress\HTMLUnit\Component
             ),
         );
         $page = $this->page(array(
+            'uri' => 'http://website.com/referred.html',
             'method' => 'POST',
             'params' => $params,
-            'server' => array('HTTP_X-Requested-With' => 'XMLHttpRequest'),
+            'cookies' => array(
+                '_bpa' => $started.'.'.time(),
+            ),
+            'server' => array(
+                'HTTP_X-Requested-With' => 'XMLHttpRequest',
+                'HTTP_USER_AGENT' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.13+ (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2',
+            ),
         ));
         $this->assertEquals(array(
             'loaded' => 2204,
@@ -130,12 +128,11 @@ class AnalyticsTest extends \BootPress\HTMLUnit\Component
         unset($info['last'], $info['started']);
         $this->assertEquals(array(
             'hits' => 1,
-            'session' => $page->session->id(),
-            'users' => array(1),
-            'offset' => 28800,
             'timezone' => 'UM9',
-            'javascript' => true,
+            'offset' => 28800,
+            'users' => array(1),
             'agent' => array(
+                'user' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.13+ (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2',
                 'robot' => null,
                 'browser' => 'Safari',
                 'version' => '5',
@@ -143,13 +140,14 @@ class AnalyticsTest extends \BootPress\HTMLUnit\Component
                 'desktop' => 'OS X',
             ),
         ), $info);
+        $page->send('');
 
         // check that everything was logged properly
         $file = file($csv);
         $this->assertCount(3, $file);
-        foreach (array('sessions', 'users', 'hits') as $table) {
-            $row = array_shift($file);
-            $this->assertEquals($table, strstr($row, ',', true));
+        foreach (array('sessions', 'users', 'hits') as $expected) {
+            list($table, $started) = str_getcsv(array_shift($file).',,,,,');
+            $this->assertEquals($expected, $table);
         }
 
         // lookup the user's session id on the next go-around
@@ -163,22 +161,26 @@ class AnalyticsTest extends \BootPress\HTMLUnit\Component
             'server' => array('HTTP_X-Requested-With' => 'XMLHttpRequest'),
         ));
         $this->assertFalse(Analytics::log());
-        $page->send('Content');
+        $page->send('');
 
-        // create new session and log an initial hit
+        // create new analytics session and log an initial hit
         $_SESSION = array();
-        session_regenerate_id();
         $page = $this->page(array('server' => array(
             'HTTP_USER_AGENT' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.13+ (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2',
             'HTTP_REFERER' => 'https://website.com/path.html?query=string', // does not get logged
         )));
         $this->assertFalse(Analytics::log());
-        $page->send($page->display('Content')); // includes analytics.js at end of <body>
+
+        // Verify analytics.js at end of <body>
+        ob_start();
+        $page->send($page->display('Content'));
+        $output = ob_get_clean();
+        $this->assertNotFalse(strpos($output, '<script src="http://website.com/bootpress-analytics-component/analytics.js"></script>'));
 
         // check that everything was logged properly
         $file = file($csv);
-        $this->assertCount(3, $file);
-        foreach (array('hits', 'analytics', 'bots') as $table) {
+        $this->assertCount(2, $file);
+        foreach (array('hits', 'bots') as $table) {
             $row = array_shift($file);
             $this->assertEquals($table, strstr($row, ',', true));
         }
@@ -193,9 +195,6 @@ class AnalyticsTest extends \BootPress\HTMLUnit\Component
         // every 3 minutes we update the database using the csv file
         clearstatcache();
         $temp = $page->file('analytics-temp.csv');
-        if (is_file($temp)) {
-            unlink($temp);
-        }
         rename($csv, $temp);
         touch($csv, (time() - 200)); // an empty file created more than 3 minutes ago
         touch($temp, (time() - 180)); // "started to processHits()" 3 minutes ago
@@ -214,24 +213,37 @@ class AnalyticsTest extends \BootPress\HTMLUnit\Component
             'tcp' => 0,
             'request' => 323,
             'response' => 143,
-            'views' => 3,
+            'views' => 1,
         ), Analytics::log());
         $this->assertFileNotExists($temp);
         $this->assertFileNotExists($csv);
         $this->assertFalse(Analytics::process()); // there is no $csv file
 
         // check database entries
-        $this->assertEquals('', $db->value('SELECT robot FROM analytic_agents WHERE id = ?', 3));
+        /*
+        print_r([
+            'analytics' => $db->all('SELECT * FROM analytics', '', 'assoc'),
+            'analytic_agents' => $db->all('SELECT * FROM analytic_agents', '', 'assoc'),
+            'analytic_bots' => $db->all('SELECT * FROM analytic_bots', '', 'assoc'),
+            'analytic_hits' => $db->all('SELECT * FROM analytic_hits', '', 'assoc'),
+            'analytic_paths' => $db->all('SELECT * FROM analytic_paths', '', 'assoc'),
+            'analytic_sessions' => $db->all('SELECT * FROM analytic_sessions', '', 'assoc'),
+            'analytic_users' => $db->all('SELECT * FROM analytic_users WHERE user_id = ?', 1, 'assoc'),
+        ]);
+        */
+        $this->assertEquals('Google', $db->value('SELECT robot FROM analytic_agents WHERE id = ?', 1));
         $this->assertEquals('', $db->value('SELECT path FROM analytic_paths WHERE id = ?', 1));
-        $this->assertEquals(4, $db->value('SELECT COUNT(*) FROM analytics'));
-        $this->assertEquals(3, $db->value('SELECT COUNT(*) FROM analytic_agents'));
-        $this->assertEquals(2, $db->value('SELECT COUNT(*) FROM analytic_bots'));
+        $this->assertEquals(1, $db->value('SELECT COUNT(*) FROM analytics'));
+        $this->assertEquals(4, $db->value('SELECT COUNT(*) FROM analytic_agents'));
+        $this->assertEquals(3, $db->value('SELECT COUNT(*) FROM analytic_bots'));
         $this->assertEquals(3, $db->value('SELECT COUNT(*) FROM analytic_hits'));
+        $this->assertEquals(2, $db->value('SELECT COUNT(*) FROM analytic_paths'));
+        $this->assertEquals(2, $db->value('SELECT COUNT(*) FROM analytic_sessions'));
         $this->assertEquals(array(
             array('session_id' => 1, 'user_id' => 1),
         ), $db->all('SELECT * FROM analytic_users WHERE user_id = ?', 1, 'assoc'));
-        
-        $output = ob_get_clean();
+        $this->assertEquals('referred', $db->value('SELECT path FROM analytic_paths WHERE id = ?', 2));
+        $this->assertEquals('Google', $db->value('SELECT robot FROM analytic_agents WHERE id = ?', 1));
     }
 
     public function testPostAndJsonMethods()
@@ -254,7 +266,7 @@ class AnalyticsTest extends \BootPress\HTMLUnit\Component
     {
         $db = Analytics::database();
         $this->assertEquals(2, $db->value('SELECT COUNT(*) FROM analytic_sessions'));
-        $this->assertEquals(4, $db->value('SELECT COUNT(*) FROM analytics'));
+        $this->assertEquals(1, $db->value('SELECT COUNT(*) FROM analytics'));
         $db->exec("UPDATE analytics SET started = strftime('%s', 'now', '-24 hours')");
         touch(Page::html()->dir['page'].'analytics.csv'); // create the file as it no longer exists
         Analytics::process();
